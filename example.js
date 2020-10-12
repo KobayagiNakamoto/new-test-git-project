b1110f21b6f54c72465f973c03f3f62cf676875b
KobayagiNakamoto <63250177+KobayagiNakamoto@users.noreply.github.com>
2020-10-12 20:21:19 +0200

    new changes

---
+++ example.js
@@ -0,0 +1,4 @@
+console.log("this is ex");
+console.log("Students");
+console.log("Semos Education");
+console.log("JavaScript");
--- test.js
+++ test.js
@@ -1,2 +1,3 @@
 console.log("Hello");
-console.log("ok")
+console.log("ok");
+console.log("Update");
